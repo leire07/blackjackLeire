a230a09ea1301d332e38ad6a7b8d734edf9c0091
Leire <lvilmar1@epsg.upv.es>
2021-05-20 12:53:10 +0200

    Implementado método ShuffleCards

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -34,6 +34,17 @@ public class Deck : MonoBehaviour
          * En principio, la posición de cada valor se deberá corresponder con la posición de faces. 
          * Por ejemplo, si en faces[1] hay un 2 de corazones, en values[1] debería haber un 2.
          */
+
+        int varAux = 0; 
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 13; j++)
+            {
+                values[varAux] = j + 1;
+                varAux++;
+            }
+        }
     }
 
     private void ShuffleCards()
@@ -42,7 +53,23 @@ public class Deck : MonoBehaviour
          * Barajar las cartas aleatoriamente.
          * El método Random.Range(0,n), devuelve un valor entre 0 y n-1
          * Si lo necesitas, puedes definir nuevos arrays.
-         */       
+         */    
+        
+
+        int rndNum; // variable para el num random
+        Sprite tempFaces;
+        int tempValues;
+
+        for (int i = 0; i < 52; i++) // bucle que recorre las 52 cartas
+        {
+            rndNum = Random.Range(0, 52); // num random
+            tempFaces = faces[0]; // tempFaces valdrá lo q valga el primer valor del array faces
+            faces[0] = faces[rndNum]; // el valor valdrá un rndnum
+            faces[rndNum] = tempFaces; // ese eleemento valdrá tempFaces
+            tempValues = values[0]; // tempvalues valdrá el valor de values primera cas
+            values[0] = values[rndNum]; // que será un numRandom
+            values[rndNum] = tempValues; // y ese valdrá tempValues
+        }
     }
 
     void StartGame()
