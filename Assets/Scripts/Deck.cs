24c44b76b95ca338880f564afe73633b1abe217d
Leire <lvilmar1@epsg.upv.es>
2021-05-23 20:12:59 +0200

    Implementado método CalculateProbabilities
    y arreglar errores

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -12,6 +12,9 @@ public class Deck : MonoBehaviour
     public Text finalMessage;
     public Text probMessage;
 
+     public Text probMessage1;
+    public Text probMessage2;
+
     public int[] values = new int[52];
     int cardIndex = 0;    
 
@@ -80,13 +83,41 @@ public class Deck : MonoBehaviour
 
     void StartGame()
     {
+
         for (int i = 0; i < 2; i++)
         {
-            PushPlayer();
             PushDealer();
-            /*TODO:
+            PushPlayer();
+            round++;
+
+        }
+
+        /*TODO:
              * Si alguno de los dos obtiene Blackjack, termina el juego y mostramos mensaje
              */
+
+        if (valuesPlayer == 21) // si el valor de las cartas del jugador vale 21
+        {
+            finalMessage.text = "Blacjack! Ganaste"; // gana el jugador
+            stickButton.interactable = false; // inhabilitamos los dos botones
+            hitButton.interactable = false;
+        }
+        else if (valuesDealer == 21) // si el valor de las cartas del dealer vale 21
+        {
+            finalMessage.text = "Blacjack! Perdiste"; // pierdes
+            stickButton.interactable = false; // inhabilitamos los dos botones
+        }
+        if (valuesPlayer > 21) // si el valor de las cartas del jugador vale más de 21
+        {
+            finalMessage.text = "Te pasaste, perdiste"; // pierdes
+            stickButton.interactable = false; // inhabilitamos los dos botones
+            hitButton.interactable = false;
+        }
+        else if (valuesDealer > 21) // si el valor de las cartas del dealer vale más de 21
+        {
+            finalMessage.text = "El dealer se pasó, ganaste"; // ganas
+            stickButton.interactable = false; // inhabilitamos los dos botones
+            hitButton.interactable = false;
         }
     }
 
@@ -98,15 +129,86 @@ public class Deck : MonoBehaviour
          * - Probabilidad de que el jugador obtenga entre un 17 y un 21 si pide una carta
          * - Probabilidad de que el jugador obtenga más de 21 si pide una carta          
          */
+
+        float probabilidad; // variable float de la probabilidad
+        int casosPosibles; // variable de casos posibles
+
+        // Teniendo la carta oculta, probabilidad de que el dealer tenga más puntuación que el jugador
+
+        if (round != 0) // si la ronda no es la primera
+        {
+            int visibleDealer = valuesDealer - cardsDealer[0];
+            casosPosibles = 13 - valuesPlayer + visibleDealer; 
+            probabilidad = casosPosibles / 13f; 
+
+            if (probabilidad > 1) 
+            {
+                probabilidad = 1; 
+            }
+            else if (probabilidad < 0)
+            {
+                probabilidad = 0; 
+            }
+
+            probMessage.text = (probabilidad * 100).ToString() + " %"; // el texto de probabilidad será la probabilidad por 100 a string
+
+        }
+
+        //Probabilidad de que el jugador obtenga más de 21 si pide una carta
+
+        float probabilidad2a;
+        int casosPosibles2; // variable casos posibles 2
+        casosPosibles2 = 13 - (21 - valuesPlayer); // esta valdrá 13 menos (21 menos el valor del jugador)
+        probabilidad2a = casosPosibles2 / 13f;
+
+        if (probabilidad2a > 1) 
+        {
+            probabilidad2a = 1;
+        }
+        else if (probabilidad2a < 0) 
+        {
+            probabilidad2a = 0;
+        }
+
+        probMessage1.text = (probabilidad2a * 100).ToString() + "%"; // mostramos la probabilidad
+
+
+        // Probabilidad de que el jugador obtenga entre un 17 y un 21 si pide una carta
+
+        float probabilidad_17; // variable para la probabilidad de llegar a 17
+        int casosPosiblesLlegar_17; // variable para casps posible
+        casosPosiblesLlegar_17 = 13 - (16 - valuesPlayer); // casos posibles valdrá 13 menos (16 menos el valuesPlayer)
+        probabilidad_17 = casosPosiblesLlegar_17 / 13f; // probabilidad de llegar a 17 valdrá los casos partido 13f
+
+        if (probabilidad_17 > 1) 
+        {
+            probabilidad_17 = 1; 
+        }
+        else if (probabilidad_17 < 0) 
+        {
+            probabilidad_17 = 0; 
+        }
+
+        float probabilidad_17_y_21 = probabilidad_17 - probabilidad2a; // probabilidad entre 17 y 21, prob de llegar a 17 menos la de 21
+
+        if (probabilidad_17_y_21 > 1) 
+        {
+            probabilidad_17_y_21 = 1; 
+        }
+        else if (probabilidad_17_y_21 < 0)
+        {
+            probabilidad_17_y_21 = 0; 
+        }
+
+        probMessage2.text = (probabilidad_17_y_21 * 100).ToString() + " %"; // mostramos la probabilidad
     }
 
     void PushDealer()
     {
-        /*TODO:
-         * Dependiendo de cómo se implemente ShuffleCards, es posible que haya que cambiar el índice.
-         */
-        dealer.GetComponent<CardHand>().Push(faces[cardIndex],values[cardIndex]);
-        cardIndex++;        
+        dealer.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]);
+        valuesDealer += values[cardIndex];
+        cardsDealer[round] = values[cardIndex];
+        cardIndex++;
     }
 
     void PushPlayer()
@@ -114,11 +216,15 @@ public class Deck : MonoBehaviour
         /*TODO:
          * Dependiendo de cómo se implemente ShuffleCards, es posible que haya que cambiar el índice.
          */
-        player.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]/*,cardCopy*/);
-        cardIndex++;
-        CalculateProbabilities();
-    }       
 
+        player.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]/*,cardCopy*/);
+        valuesPlayer += values[cardIndex];
+        cardsPlayer[round] = values[cardIndex];
+        cardIndex++;
+
+        CalculateProbabilities();
+    }             
+    
     public void Hit()
     {
         /*TODO: 
@@ -130,22 +236,27 @@ public class Deck : MonoBehaviour
 
         /*TODO:
          * Comprobamos si el jugador ya ha perdido y mostramos mensaje
-         */    
-        
+         */
+
         if (valuesPlayer > 21) // si el valor del jugador es mayor que 21
         {
-            finalMessage.text = "Game over, you've passed"; // pierdes
+            finalMessage.text = "Te pasaste, perdiste"; // pierdes
             stickButton.interactable = false; // inhabilitamos los botones
             hitButton.interactable = false;
 
         }
+
+        /*TODO:
+       * Comprobamos si el jugador ya ha perdido y mostramos mensaje
+       */
 
         else if (valuesPlayer == 21) // también si el valor del jugador vale 21
         {
-            finalMessage.text = "Blacjack, you win!"; // ganas
+            finalMessage.text = "Blacjack! Ganaste"; // ganas
             stickButton.interactable = false; // inhabilitamos los botones
             hitButton.interactable = false;
         }
+      
 
     }
 
@@ -196,14 +307,17 @@ public class Deck : MonoBehaviour
 
     public void PlayAgain()
     {
-        hitButton.interactable = true;
+        hitButton.interactable = true; // habilitamos los botones
         stickButton.interactable = true;
         finalMessage.text = "";
         player.GetComponent<CardHand>().Clear();
-        dealer.GetComponent<CardHand>().Clear();          
+        dealer.GetComponent<CardHand>().Clear();
         cardIndex = 0;
-        ShuffleCards();
-        StartGame();
+        valuesPlayer = 0;
+        round = 0;
+        valuesDealer = 0;
+        ShuffleCards(); // mezclamos
+        StartGame(); // empezamos juego
     }
     
 }
